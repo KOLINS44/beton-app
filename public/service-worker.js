f116ef0942a50c17c2753abf93d950d0dc3173ec
KOLINS44 <wwwqqq_3@mail.ru>
2025-06-04 17:56:03 +0300

    Initial implementation of concrete orders app

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'beton-cache-v1';
+const URLsToCache = ['/', '/public/styles.css', '/public/app.js', '/public/zayavki.js'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
